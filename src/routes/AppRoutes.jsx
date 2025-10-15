7b608c71df338cb7d31485135294ccf7686f6a0f
alfar-programer <alfar.programer@gmail.com>
2025-10-16 00:24:16 +0300

    Your commit message

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,48 +1,30 @@
-// // src/routes/AppRoutes.jsx
-// import React from 'react';
-// import { Routes, Route } from 'react-router-dom';
-// import Home from '../pages/Home/Home';
-// import About from '../pages/About/About';        // ✅
-// import Projects from '../pages/Projects/Projects';  // ✅
-// import Contact from '../pages/Contact/Contact';    // ✅
-
-// const AppRoutes = () => {
-//   return (
-//     <Routes>
-//       <Route path="/" element={<Home />} />
-//       <Route path="/about" element={<About />} />
-//       <Route path="/projects" element={<Projects />} />
-//       <Route path="/contact" element={<Contact />} />
-//     </Routes>
-//   );
-// };
-
-// export default AppRoutes;
-
-// src/routes/AppRoutes.jsx
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
+import { useMediaQuery } from 'react-responsive';
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import Projects from '../pages/Projects/Projects';
 import Contact from '../pages/Contact/Contact';
 import Navbar from '../components/Ui/Navbar/Navbar';
-import AnimationCanva from '../components/AnimationCanvas'
+import AnimationCanva from '../components/AnimationCanvas';
 import Footer from '../components/Ui/Footer/Footer';
 
 const AppRoutes = () => {
+  const isMobile = useMediaQuery({ query: '(max-width: 1024px)' });
+
   return (
     <>
-      <AnimationCanva />
-      <Navbar />     
-        <Routes>
-          <Route path="/Home" element={<Home />} />
-          <Route path="/about" element={<About />} />
-          <Route path="/projects" element={<Projects />} />
-          <Route path="/contact" element={<Contact />} />
-          <Route path="*" element={<Home />} />
-        </Routes>
-        <Footer />
+      {!isMobile && <AnimationCanva />}
+
+      <Navbar />
+      <Routes>
+        <Route path="/Home" element={<Home />} />
+        <Route path="/about" element={<About />} />
+        <Route path="/projects" element={<Projects />} />
+        <Route path="/contact" element={<Contact />} />
+        <Route path="*" element={<Home />} />
+      </Routes>
+      <Footer />
     </>
   );
 };
