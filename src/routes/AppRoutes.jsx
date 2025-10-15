84a15975bcad635dd0a3c48e8a4794a7ed5c6c73
alfar-programer <alfar.programer@gmail.com>
2025-10-16 00:37:14 +0300

    Your commit message

--- src/routes/AppRoutes.jsx
+++ src/routes/AppRoutes.jsx
@@ -1,20 +1,18 @@
 import React from 'react';
 import { Routes, Route } from 'react-router-dom';
-import { useMediaQuery } from 'react-responsive';
 import Home from '../pages/Home/Home';
 import About from '../pages/About/About';
 import Projects from '../pages/Projects/Projects';
 import Contact from '../pages/Contact/Contact';
 import Navbar from '../components/Ui/Navbar/Navbar';
-import AnimationCanva from '../components/AnimationCanvas';
 import Footer from '../components/Ui/Footer/Footer';
 
 const AppRoutes = () => {
-  const isMobile = useMediaQuery({ query: '(max-width: 1024px)' });
+  
 
   return (
     <>
-      {!isMobile && <AnimationCanva />}
+     
 
       <Navbar />
       <Routes>
